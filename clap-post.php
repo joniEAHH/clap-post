88b6d32c761c2b1cf93c80602464666094f2134a
Joni Dores <paulojfigueiredo27@gmail.com>
2019-01-14 20:10:12 +0000

    first commit

---
+++ clap-post.php
@@ -0,0 +1,81 @@
+<?php
+   /*
+   Plugin Name: Clap post
+   Plugin URI: http://my-awesomeness-emporium.com
+   description: A plugin to create awesomeness and spread joy
+   Version: 1.2
+   Author: Joni Dores
+   Author URI: https://jonidores.com
+   License: GPL2
+   */
+?>
+
+<?php
+add_action("wp_ajax_user_vote", "user_vote");
+add_action("wp_ajax_nopriv_user_vote", "user_vote");
+
+function user_vote() {
+
+   if ( !wp_verify_nonce( $_REQUEST['nonce'], "user_vote_nonce")) {
+      exit("No naughty business please");
+   }   
+
+   $vote_count = get_post_meta($_REQUEST["post_id"], "votes", true);
+   $vote_count = ($vote_count == '') ? 0 : $vote_count;
+   $new_vote_count = $vote_count + 1;
+
+   $vote = update_post_meta($_REQUEST["post_id"], "votes", $new_vote_count);
+
+   if($vote === false) {
+      $result['type'] = "error";
+      $result['vote_count'] = $vote_count;
+   }
+   else {
+      $result['type'] = "success";
+      $result['vote_count'] = $new_vote_count;
+   }
+
+   if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+      $result = json_encode($result);
+      echo $result;
+   }
+   else {
+      header("Location: ".$_SERVER["HTTP_REFERER"]);
+   }
+
+   die();
+
+}
+
+
+
+
+//add_action( 'get_footer', 'is_subpage' );
+    
+
+function my_script_enqueuer() {
+    wp_register_script( "voter_script", WP_PLUGIN_URL.'/clap-post/voter_script.js', array('jquery') );
+    wp_localize_script( 'voter_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
+    if(get_post_type() == 'blog') {
+    wp_enqueue_script( 'voter_script' );
+    }
+}
+
+function is_subpage() {
+    global $post;                              // load details about this page
+    var_dump($post);
+    if(get_post_type() == 'blog'):
+        ?>
+        <script>
+            console.log("true");
+        </script> 
+        <?php
+     //   wp_register_script( "voter_script", WP_PLUGIN_URL.'/clap-post/voter_script.js', array('jquery') );
+    //    wp_localize_script( 'voter_script', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
+     
+     //   wp_enqueue_script( 'voter_script' );
+    endif;
+}  
+
+?>
+
---
+++ voter_script.js
@@ -0,0 +1,108 @@
+var clickVote = document.querySelectorAll('.user-vote-js');
+var voteCount = document.querySelectorAll(".vote-count-js");
+var voteText = document.querySelectorAll(".vote-text-js");
+
+
+var clicks = 0;
+var quickAdd = false;
+
+function mouseUp() {
+    quickAdd = setTimeout(doSetTimeout, 2500);
+}
+
+function mouseDown() {
+    clearTimeout(quickAdd);
+}
+
+function doSetTimeout(i) {
+    for (var i = 0; i < voteCount.length; i++) {
+        voteCount[i].classList.remove("is-active");
+        voteText[i].classList.remove("hidden");
+    }
+}
+
+for (var i = 0; i < clickVote.length; i++) {
+
+    var self = clickVote[i];
+
+    self.addEventListener('click', function (event) {
+
+        // prevent browser's default action
+        event.preventDefault();
+        console.log("clicked")
+        // call your awesome function here
+        userVote(this); // 'this' refers to the current button on for loop
+
+    }, false);
+}
+
+
+
+
+function userVote() {
+
+    clicks++;
+    for (var i = 0; i < voteCount.length; i++) {
+        if (clicks <= 50) {
+            //console.log(voteCount[i]);
+            voteCount[i].innerHTML = "+" + clicks;
+        }
+
+        // removing the class
+        clickVote[i].classList.remove("click-vote--animation");
+        // re-adding the class
+        void clickVote[i].offsetWidth;
+        clickVote[i].classList.add("click-vote--animation");
+        clickVote[i].classList.add("is-active");
+
+        // hide vote text on click
+        voteText[i].classList.add("hidden");
+        // show vote counter after cick
+        voteCount[i].classList.add("is-active");
+
+    }
+
+
+
+
+}
+
+
+
+var voteCounter = document.querySelectorAll(".vote-counter-js");
+
+jQuery(clickVote).on("click", function (e) {
+
+    e.preventDefault();
+
+    if (clicks == 50) {
+       return false;
+    }  
+
+    post_id = jQuery(this).attr("data-post_id")
+    nonce = jQuery(this).attr("data-nonce")
+
+    jQuery.ajax({
+        type: "post",
+        dataType: "json",
+        url: myAjax.ajaxurl,
+        data: {
+            action: "user_vote",
+            post_id: post_id,
+            nonce: nonce
+        },
+        success: function (response) {
+            if (response.type == "success") {
+
+                jQuery(voteCounter).each(function () {
+                    jQuery(this).html(response.vote_count);
+                });
+
+                //  console.log(response);
+            } else {
+                console.log(response);
+            }
+        }
+    })
+
+})
